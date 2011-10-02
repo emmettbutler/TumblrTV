963a59bbf1c96dd65840ff3176be7fd912337ac6
Emmett Butler <emmett.butler321@gmail.com>
2011-10-01 20:10:50 -0400

    tumblr_auth added

---
+++ tumblr_auth/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'Ne3RcPKi5aNSGEvz4VYuohw1mAbQR3cnHjrSYjRZJvLDHkWuXD');
+define('CONSUMER_SECRET', 'r2lTPG0xBNWkHvUXMdfFunOlFjMWWh5y4wiqSxK33XugXIn5r4');
+define('OAUTH_CALLBACK', 'http://localhost/hackny/tumblr_auth/tumblroauth/callback.php');
