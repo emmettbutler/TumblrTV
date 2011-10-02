07dee7265b59674e470ef56b6b43418a2f99538a
Emmett Butler <emmett.butler321@gmail.com>
2011-10-01 20:44:32 -0400

    get feed

---
+++ authenticate.php
@@ -0,0 +1,5 @@
+<form action="http://www.tumblr.com/api/dashboard" method="POST">
+	<input type="text" name="email" value="email" />
+	<input type="password" name="password" value="email" />
+	<input type="submit" value="Submit" />
+</form>
