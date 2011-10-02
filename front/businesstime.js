1f268db1f2f3fe0555c41c296ec3e6e7ec2df012
Yuriy Skobov <stokedover9k@gmail.com>
2011-10-02 02:37:59 -0400

    merge

--- front/businesstime.js
+++ front/businesstime.js
@@ -7,13 +7,13 @@ var selected = 0;
 var items = ['#user', '#pass'];
 
 function getNext() {
-  if ((cur_pos + 1) < playlist.length)
-    cur_pos++;
+    if ((cur_pos + 1) < playlist.length)
+	cur_pos++;
 }
 
 function getPrevious() {
-  if (cur_pos > 0)
-    cur_pos--;
+    if (cur_pos > 0)
+	cur_pos--;
 }
 
 function play() {
@@ -21,91 +21,113 @@ function play() {
 }
 
 function login() {
-  user = $('#user').val();
-  pass = $('#pass').val();
-  $.post('http://www.tumblr.com/api/dashboard/json?type=video', 
+    user = $('#user').val();
+    pass = $('#pass').val();
+    /*
+      $.post('http://www.tumblr.com/api/dashboard/json?type=video', 
       {email: user, password: pass}, function(json) {
-    eval(json);
-    playlist = tumblr_api_read.posts;
-    play();
-  });
-  hide('#login', 100);  
-  return false;
+      if( json == null ) alert("null");
+      else if( json == "" ) alert("empty");
+      else alert("not");
+      eval(json);
+      alert("1");
+      playlist = tumblr_api_read.posts;
+      alert("2");
+      play();
+      });
+    */
+    $.ajax({
+	type      : 'POST',
+	//url       : 'www.tumblr.com/api/dashboard/json?type=video',
+	url       : "www.twitter.com",
+	data      : "email=emmett.butler321@gmail.com&password=April16!",
+	success   : function(data) {
+
+	    alert(data);
+	},
+	error     : function(msg) {
+	    alert("error");
+	}
+    });
+    
+    
+    hide('#login', 100);  
+    return false;
 }
 
 function hide(selector, ms) {
-  $(selector).animate({opacity: 0}, ms, function() {
-    $(selector).addClass("hidden");
-  });
+    $(selector).animate({opacity: 0}, ms, function() {
+	$(selector).addClass("hidden");
+    });
 }
 
 function show(selector, ms) {
-  $(selector).removeClass("hidden").animate({opacity: 1}, ms);
+    $(selector).removeClass("hidden").animate({opacity: 1}, ms);
 }
 
 function getKey(key){
-  if ( key == null ) {
-    keycode = event.keyCode;
-    // To Mozilla
-  } else {
-    keycode = key.keyCode;
-  }
-  // Return the key in lower case form
-  return keycode;
+    if ( key == null ) {
+	keycode = event.keyCode;
+	// To Mozilla
+    } else {
+	keycode = key.keyCode;
+    }
+    // Return the key in lower case form
+    return keycode;
 }
 
 function up() {
-  if (stage == 0) {
-    selected--;
-    if (selected < 0)
-      selected = 0;
-    $(items[selected]).select();
-  }
-  else {
-    getPrevious();
-    play();
-  }
+    if (stage == 0) {
+	selected--;
+	if (selected < 0)
+	    selected = 0;
+	$(items[selected]).select();
+    }
+    else {
+	getPrevious();
+	play();
+    }
 }
 
 function down() {
-  if (stage == 0) {
-    selected++;
-    if (selected >= items.length)
-      selected = items.length - 1;
-    $(items[selected]).select();
-  }
-  else {
-    getNext();
-    play();
-  }
+    if (stage == 0) {
+	selected++;
+	if (selected >= items.length)
+	    selected = items.length - 1;
+	$(items[selected]).select();
+    }
+    else {
+	getNext();
+	play();
+    }
 }
 
 function left() {
-  if (stage == 1) {
-    $('#player').addClass('hidden');
-    show('#login', 100);
+    if (stage == 1) {
+	$('#player').addClass('hidden');
+	show('#login', 100);
 	cur_pos = 0;
-    stage = 0;
-  }
+	stage = 0;
+    }
 }
 
 function right() {
-  if (stage == 0) {
-    stage = 1;
-    login();
-  }
+    if (stage == 0) {
+	stage = 1;
+	login();
+    }
 }
 
 $(function() {
-  $('#user').select();
-  $('#submit').click(login);
+    $('#user').select();
+    $('#submit').click(login);
 
-  $(document).keydown(function (eh) {
-    var keycode = getKey(eh);
-    if (keycode == 37) left();
-    else if (keycode == 38) up();
-    else if (keycode == 39) right();
-    else if (keycode == 40) down();
-  });
+    $(document).keydown(function (eh) {
+	var keycode = getKey(eh);
+	if (keycode == 37) left();
+	else if (keycode == 38) up();
+	else if (keycode == 39) right();
+	else if (keycode == 40) down();
+    });
 
 });
