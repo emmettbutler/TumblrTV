382d2aa6cca88d15fe4f124b86ad84945493adc7
Eric Sluyter <wondersluyter@gmail.com>
2011-10-02 03:49:31 -0400

    fixing video loading

--- front/get-bullshit.php
+++ front/get-bullshit.php
@@ -2,7 +2,7 @@
 // pass $_GET[email] and $_GET[password]
 
 
-$url = "curl --data 'email={$_GET[email]}&password={$_GET[password]}' http://www.tumblr.com/api/dashboard/json?type=video";
+$url = "curl --data 'email={$_[email]}&password={$_GET[password]}' http://www.tumblr.com/api/dashboard/json?type=video";
 
 echo shell_exec($url);
 ?>
