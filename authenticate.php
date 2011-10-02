c7fa03794b73ba1bca173d1e92d9f53521faa162
Yuriy Skobov <stokedover9k@gmail.com>
2011-10-01 21:27:46 -0400

    merge

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,5 @@
-<form action="http://www.tumblr.com/api/dashboard" method="POST">
-	<input type="text" name="email" value="email" />
-	<input type="password" name="password" value="email" />
+<form action="http://www.tumblr.com/api/dashboard/json" method="POST">
+	<input type="text" name="email" value="amk528@cs.nyu.edu" />
+	<input type="password" name="password" value="ListenrTest" />
 	<input type="submit" value="Submit" />
 </form>
