b990fd781f83a34af2caf1d8170b14b770a0bef6
Emmett Butler <emmett.butler321@gmail.com>
2011-10-01 21:04:40 -0400

    removed auth

--- authenticate.php
+++
@@ -1,5 +0,0 @@
-<form action="http://www.tumblr.com/api/dashboard/json?type=video" method="POST">
-	<input type="text" name="email" value="email" />
-	<input type="password" name="password" value="April16!" />
-	<input type="submit" value="Submit" />
-</form>
